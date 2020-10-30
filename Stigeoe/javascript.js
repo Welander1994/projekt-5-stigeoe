db8fe06efd0b78d19f289a9ae3479eabdff62fa7
idax6797 <idax6797@edu.ucl.dk>
2020-10-30 14:34:53 +0100

    Ord slider på banner

--- Stigeoe/javascript.js
+++ Stigeoe/javascript.js
@@ -1,9 +1,30 @@
 function burgerMenu() {
-    var element = document.getElementById("toggle1");
+    let element = document.getElementById("toggle1");
     element.classList.toggle("menu-active");
 }
 
 function dropdown() {
-    var element = document.getElementById("toggle2");
+    let element = document.getElementById("toggle2");
     element.classList.toggle("undermenu-active");
 }
+
+//banner ord slide animation
+let ordSlider_i = 0;
+let ordSlider_array = ["aktiviteternes ø", "familiens ø", "børnenes ø", "den aktives ø", "en tumleplads"];
+let ordSlider_elem;
+
+function ordSliderNext() {
+    ordSlider_i++;
+    ordSlider_elem.style.opacity = 0;
+    if (ordSlider_i > (ordSlider_array.length - 1)) {
+        ordSlider_i = 0;
+    }
+    setTimeout('ordSliderSlide()', 1000);
+}
+function ordSliderSlide() {
+    ordSlider_elem.innerHTML = ordSlider_array[ordSlider_i];
+    ordSlider_elem.style.opacity = 1;
+    setTimeout('ordSliderNext()', 2000);
+}
+
+ordSlider_elem = document.getElementById("ordSlider"); ordSliderSlide(); 
