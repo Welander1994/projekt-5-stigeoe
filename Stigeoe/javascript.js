7bf7960352048495345cd8a4bb0a49fc0780c531
Welander1994 <64834767+Welander1994@users.noreply.github.com>
2020-11-05 13:31:06 +0100

    updateret javascript
    samlet det kode som kun skal køre på forside og ligeså med ting som skal køre på alle sider

--- Stigeoe/javascript.js
+++ Stigeoe/javascript.js
@@ -10,111 +10,85 @@ function dropdown() {
     element.classList.toggle("undermenu-active");
 }
 
-//Countdowns
+let acc = document.getElementsByClassName("harmonika");
+let i;
 
-// Set the date we're counting down to
-var countDownDate = new Date("dec 1, 2020 13:00:0").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function () {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var timer = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-
-    // Output the result in an element with id="countdown"
-    document.getElementById("countdown").innerHTML = days + "d " + timer + "t ";
-
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "Datoen for dette event er desvære overskredet";
-    }
-}, 1000);
-
-// Set the date we're counting down to
-var countDownDate2 = new Date("feb 20, 2021 10:00:0").getTime();
-
-// Update the count down every 1 second
-var y = setInterval(function () {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate2 - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var timer = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-
-    // Output the result in an element with id="countdown"
-    document.getElementById("countdown2").innerHTML = days + "d " + timer + "t ";
-
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(y);
-        document.getElementById("countdown2").innerHTML = "Datoen for dette event er desvære overskredet";
-    }
-}, 1000);
-
-// Set the date we're counting down to
-var countDownDate3 = new Date("apr 04, 2021 10:00:0").getTime();
-
-// Update the count down every 1 second
-var z = setInterval(function () {
-
-    // Get today's date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate3 - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var timer = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-
-    // Output the result in an element with id="countdown"
-    document.getElementById("countdown3").innerHTML = days + "d " + timer + "t ";
-
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(z);
-        document.getElementById("countdown3").innerHTML = "Datoen for dette event er desvære overskredet";
-    }
-}, 1000);
-
-
-//banner ord slide animation
-let ordSlider_i = 0;
-let ordSlider_array = [" aktiviteternes ø", " familiens ø", " børnenes ø", " den aktives ø", " en tumleplads"];
-let ordSlider_elem;
-
-
-function ordSliderNext() {
-    ordSlider_i++;
-    ordSlider_elem.style.opacity = 0;
-    if (ordSlider_i > (ordSlider_array.length - 1)) {
-        ordSlider_i = 0;
-    }
-    setTimeout('ordSliderSlide()', 1000);
-}
-function ordSliderSlide() {
-    ordSlider_elem.innerHTML = "&nbsp" + ordSlider_array[ordSlider_i];
-    ordSlider_elem.style.opacity = 1;
-    setTimeout('ordSliderNext()', 2000);
-
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("activeharmonika");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
 
-ordSlider_elem = document.getElementById("ordSlider"); ordSliderSlide();
+
+
+
+
+var underMenu = ["FORSIDE", "BOOKING", "KONTAKT", "GALLERI", "kort", "HISTORIE", "NATUR", "Fremtid", "FAQ"];
+
+
+function transitionForside() {
+    document.getElementById("transitionText").innerHTML = underMenu[0];
+}
+function transitionBooking() {
+    document.getElementById("transitionText").innerHTML = underMenu[1];
+}
+function transitionKontakt() {
+    document.getElementById("transitionText").innerHTML = underMenu[2];
+}
+function transitionGalleri() {
+    document.getElementById("transitionText").innerHTML = underMenu[3];
+
+}
+function transitionKort() {
+    document.getElementById("transitionText").innerHTML = underMenu[4];
+}
+function transitionHistorie() {
+    document.getElementById("transitionText").innerHTML = underMenu[5];
+}
+function transitionNatur() {
+    document.getElementById("transitionText").innerHTML = underMenu[6];
+}
+function transitionFremtid() {
+    document.getElementById("transitionText").innerHTML = underMenu[7];
+}
+function transitionFAQ() {
+    document.getElementById("transitionText").innerHTML = underMenu[8];
+}
+
+
+
+window.onload = () => {
+    const transition_el = document.querySelector(".transition")
+    const anchors = document.querySelectorAll("li a");
+
+    setTimeout(() => {
+        transition_el.classList.remove("is-active");
+
+
+    }, 700);
+
+    for (let i = 0; i < anchors.length; i++) {
+        const anchor = anchors[i];
+
+        anchor.addEventListener("click", e => {
+            e.preventDefault();
+            let target = e.target.href;
+
+            transition_el.classList.add("is-active");
+
+            setTimeout(() => {
+                window.location.href = target;
+                console.log(target);
+            }, 700)
+        });
+    }
+
+}
 
 
